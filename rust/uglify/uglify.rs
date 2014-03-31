48917305249b2427a79cc0652a32935c38f9a9ed
Alan Andrade <alan.andradec@gmail.com>
2014-03-31 00:52:40 -0700

    beta of parallel uglify

--- rust/uglify/uglify.rs
+++ rust/uglify/uglify.rs
@@ -2,36 +2,70 @@
 // concurrency.
 //
 
-use std::io::fs::walk_dir;
-use std::io::Process;
-use std::str;
+use std::io::fs::{walk_dir, File};
+use std::io::{Process,TempDir};
 
-static CORES: uint = 4;
+//static CORES: uint = 4;
 
 fn main () {
-    // Locate the javascript files.
     let js_dir = Path::new("js");
+    let mut paths = walk_dir(&js_dir).unwrap();
+    let mut uglies = Vec::new();
 
-    // Fake developer testing framework
-    for path in walk_dir(&js_dir).unwrap() {
-        println!("{}", path.display());
+    for path in paths {
+        uglies.push(UProc::new([path.as_str().unwrap().to_owned()]));
     }
 
-    // Ok, now split the job.
-    let mut paths = walk_dir(&js_dir).unwrap();
-    let size = paths.len();
+    let mut file = File::create(&Path::new("mylib.js"));
 
-    // Asume you have 4 cores
-    let per_core = size / CORES;
+    for ugly in uglies.mut_iter() {
+        ugly.ps.wait();
+        file.write(ugly.read());
+    }
+}
 
-    // Fake developer assurance
-    println!("per core: {}", per_core);
+struct TempFile {
+    file: File
+}
 
-    let mut process = match Process::new("uglifyjs", &[~"js/d3.js"]) {
-        Ok(p) => p,
-        Err(e) => fail!("failed to execute process: {}", e)
-    };
+impl TempFile {
+    fn new() -> TempFile {
+        let temp_path = TempDir::new(&"split").unwrap();
+        let temp_file_name = temp_path.path().as_str().unwrap() + "test";
+        let temp_file_path = Path::new(temp_file_name);
 
-    let proc_output = process.wait_with_output();
-    print!("output: {:s}", str::from_utf8_owned(proc_output.output).unwrap());
+        let temp_file = match File::create(&temp_file_path) {
+            Ok(f) => f,
+            Err(e) => fail!("{}", e)
+        };
+
+        TempFile { file: temp_file }
+    }
+
+    fn fullname(&self) -> ~str {
+        self.file.path().as_str().unwrap().to_owned()
+    }
+}
+
+struct UProc {
+    file: TempFile,
+    ps: Process
+}
+
+impl UProc {
+    fn new (load: &[~str]) -> ~UProc {
+        let file = TempFile::new();
+        let mut options = vec!(~"-me", ~"-o", file.fullname());
+        options.push_all(load);
+
+        match Process::new("uglifyjs", options.as_slice()) {
+            Ok(ps) => { ~UProc { file: file, ps: ps } },
+            Err(e) => { fail!("\nPlease install uglifyjs with 'npm -g install ugligy-js'.\n{}", e) }
+        }
+    }
+
+    fn read(&self) -> ~[u8] {
+        let mut reader = File::open(self.file.file.path()).unwrap();
+        reader.read_to_end().unwrap()
+    }
 }
