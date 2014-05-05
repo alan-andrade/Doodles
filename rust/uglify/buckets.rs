f10bcef7f733289c670d63478dc6792c9755d39d
Alan Andrade <alan.andradec@gmail.com>
2014-05-05 08:43:41 -0700

    Buckets work now, but still need to tweak the queue

--- rust/uglify/buckets.rs
+++ rust/uglify/buckets.rs
@@ -1,47 +1,48 @@
-#![crate_id = "buckets#0.0.1"]
-
 extern crate manifesto;
-//use std::sync::mpsc_queue::{Queue, Data};
-//use std::sync::arc::UnsafeArc;
-//use std::comm::channel;
+
+use std::sync::mpsc_queue::{Queue, Data, Empty, Inconsistent};
+use std::sync::arc::UnsafeArc;
+use std::comm::channel;
+use std::io::Process;
 use manifesto::Manifesto;
 
 fn main () {
-    if !Manifesto::check() {
-        fail!("Please create a manifesto.json");
+    let manifesto = Manifesto::new("manifesto.json");
+    let file_chunks = manifesto.split(1);
+
+    let (tx, rx) = channel();
+    let queue = Queue::new();
+    let arc_queue = UnsafeArc::new(queue);
+
+    for files in file_chunks.iter() {
+        let filenames = pluck_filenames(*files);
+
+        let arc_queue = arc_queue.clone();
+        spawn(proc() {
+            unsafe {
+                (*arc_queue.get()).push(Process::new("uglifyjs", filenames))
+            }
+        });
     }
 
-    let manifesto = Manifesto::new();
-    let cores = 4;
-
-    for files in manifesto.split(cores).iter() {
-        println!("{:?}", files);
-        println!("====");
+    let mut i = 0;
+    while i < file_chunks.len() {
+        match unsafe { (*arc_queue.get()).pop() } {
+            Data(d) => {
+                i = i + 1;
+                tx.send(d);
+            },
+            Empty|Inconsistent => {}
+        }
     }
 
-    //let (tx, ry) = channel();
-    //let mut q = Queue::new();
+    for _i in range(0, file_chunks.len()) {
+        let output = rx.recv().unwrap().id();//.stdout.get_mut_ref().read_to_end();
+        println!("{:?}", output);
+    }
 
-    //let q = UnsafeArc::new(q);
+}
 
-    //tx.send(1);
-    //ry.recv();
-
-    //let q1 = q.clone();
-    //spawn(proc() {
-        //unsafe { (*q1.get()).push(1); }
-    //});
-
-    //let q1 = q.clone();
-    //spawn(proc() {
-        //unsafe { (*q1.get()).push(2); }
-    //});
-
-    //for i in range(0,5) {
-        //let z = unsafe { (*q.get()).pop() };
-        //match z {
-            //Data(d) => { println!("{}", d) },
-            //_ => { println!("no match {}", i+1) }
-        //}
-    //}
+fn pluck_filenames (files: &[Path]) -> ~[~str] {
+    files.iter().map(|f| f.as_str().unwrap().to_owned()).collect::<~[~str]>()
 }
